9c133741bff6818a43d21d51e5b728ab21f95db5
dolphin-pc <pcx474@gmail.com>
2024-09-05 11:04:21 +0900

    Revert "[2024-09-05] 박찬영 [BOJ] 12851번, 숨바꼭질 2 #64"
    This reverts commit 33b4e72871f293cbca5787bfede95b85ba1aefe3.

--- Main.java
+++ Main.java
@@ -1,61 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.Arrays;
 
-public class Main {
-    static int[] visit = new int[100001];
-    static int N, K, fastest_time = Integer.MAX_VALUE, fastest_ways = 0;
+// public class Main {
+// static int[] arr;
+// static int n, s, result = 0;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
+// public static void main(String[] args) throws IOException {
+// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// String[] input = br.readLine().split(" ");
 
-        N = Integer.parseInt(input[0]);
-        K = Integer.parseInt(input[1]);
+// n = Integer.parseInt(input[0]);
+// s = Integer.parseInt(input[1]);
 
-        for (int i = 0; i < visit.length; i++) {
-            visit[i] = Integer.MAX_VALUE;
-        }
+// arr = Arrays.stream(br.readLine().split("
+// ")).mapToInt(Integer::parseInt).toArray();
 
-        bfs();
+// backtraking(0, 0);
 
-        System.out.println(fastest_time);
-        System.out.println(fastest_ways);
-    }
+// System.out.println(result);
+// }
 
-    static void bfs() {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{N, 0});
-        visit[N] = 0;
+// public static void backtraking(int idx, int sum) {
+// int newSum = sum + arr[idx];
 
-        while (!q.isEmpty()) {
-            int[] current = q.poll();
-            int pos = current[0];
-            int time = current[1];
+// if (newSum == s) {
+// result++;
+// }
 
-            if (fastest_time < time) break;
+// if (idx == n - 1) {
+// return;
+// }
 
-            if (pos == K) {
-                if (fastest_time > time) {
-                    fastest_ways = 0;
-                }
-
-                fastest_time = time;
-                fastest_ways++;
-                continue;
-            }
-
-            int[] nextPositions = {pos - 1, pos + 1, pos * 2};
-            for (int next : nextPositions) {
-                if (next < 0 || next > 100000) continue;
-
-                if (visit[next] < time + 1) continue;
-
-                visit[next] = time + 1;
-                q.add(new int[]{next, time + 1});
-            }
-        }
-    }
-}
+// backtraking(idx + 1, newSum);
+// backtraking(idx + 1, sum);
+// }
+// }
